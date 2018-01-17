cfbf7c1b7cf31cb2e6315a814df1543d80b01433
Nardi <fernandonardi87@gmail.com>
2018-01-17 21:52:03 -0200

    first commit

---
+++ src/variables.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var str, str1 string
+	var str string
+	var integer int
+	var boolean bool
+
+	str = "test"
+	integer = 10
+	boolean = true
+
+	fmt.Printf("str = %s\n", str)
+	fmt.Printf("integer = %d\n", integer)
+	fmt.Println(boolean)
+}
