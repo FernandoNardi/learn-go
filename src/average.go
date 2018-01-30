7abd20167415cd993b3b27f490fefb878ced9150
Nardi <fernandonardi87@gmail.com>
2018-01-30 21:42:15 -0200

    udemy - conversions of types (2)

---
+++ src/average.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var sum int = 17
+
+	var quantity int = 5
+
+	var average float32
+
+	average = float32(sum) / float32(quantity)
+
+	fmt.Println(average)
+
+}
